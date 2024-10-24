7ac35c146cc8260baf211606bf909d2cdbe6946e
bigshcool <990194923@qq.com>
2024-10-25 02:42:48 +0800

    deploy

--- src/.vuepress/.temp/blog/type.js
+++ src/.vuepress/.temp/blog/type.js
@@ -1,13 +1,2 @@
-export const typesMap = {"article":{"/":{"path":"/article/","indexes":[6,7,8,9,10,11,12,13,14,24,0,19,18,17,27,28,20,21,22,23,25,26,29,1,2,5,3,4,15,16]}},"star":{"/":{"path":"/star/","indexes":[20,6,7,8,9,10,11,12,13,14,24,25,0]}},"timeline":{"/":{"path":"/timeline/","indexes":[6,7,8,9,10,11,12,13,14,24,19,18,17,27,28,20,21,22,23,25,26,0]}}};
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept();
-  if (__VUE_HMR_RUNTIME__.updateBlogType)
-    __VUE_HMR_RUNTIME__.updateBlogType(typesMap);
-}
-
-if (import.meta.hot)
-  import.meta.hot.accept(({ typesMap }) => {
-    __VUE_HMR_RUNTIME__.updateBlogType(typesMap);
-  });
+export const typesMap = {"article":{"/":{"path":"/article/","indexes":[6,7,8,9,10,11,12,13,14,24,0,29,1,2,5,3,4,15,16,19,18,17,27,28,20,21,22,23,25,26]}},"star":{"/":{"path":"/star/","indexes":[20,6,7,8,9,10,11,12,13,14,24,25,0]}},"timeline":{"/":{"path":"/timeline/","indexes":[29,1,2,5,3,4,15,16,6,7,8,9,10,11,12,13,14,24,19,18,17,27,28,20,21,22,23,25,26,0]}}};
 
--- src/.vuepress/.temp/pages/tag/基础/index.html.js
+++ src/.vuepress/.temp/pages/tag/基础/index.html.js
@@ -1,16 +1,3 @@
 import comp from "F:/code_project/VueProject/blog/blog/src/.vuepress/.temp/pages/tag/基础/index.html.vue"
-const data = JSON.parse("{\"path\":\"/tag/%E5%9F%BA%E7%A1%80/\",\"title\":\"标签: 基础\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"标签: 基础\",\"dir\":{\"index\":false},\"index\":false,\"feed\":false,\"sitemap\":false,\"blog\":{\"type\":\"category\",\"name\":\"基础\",\"key\":\"tag\"},\"layout\":\"BlogCategory\",\"gitInclude\":[],\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://mister-hope.github.io/blog/tag/%E5%9F%BA%E7%A1%80/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"博客演示\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"标签: 基础\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"WebPage\\\",\\\"name\\\":\\\"标签: 基础\\\"}\"]]},\"headers\":[],\"readingTime\":{\"minutes\":0,\"words\":0},\"filePathRelative\":null,\"excerpt\":\"\"}")
+const data = JSON.parse("{\"path\":\"/tag/%E5%9F%BA%E7%A1%80/\",\"title\":\"标签: 基础\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"标签: 基础\",\"dir\":{\"index\":false},\"index\":false,\"feed\":false,\"sitemap\":false,\"blog\":{\"type\":\"category\",\"name\":\"基础\",\"key\":\"tag\"},\"layout\":\"BlogCategory\",\"gitInclude\":[],\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://mister-hope.github.io/blog/tag/%E5%9F%BA%E7%A1%80/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"博客演示\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"标签: 基础\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"WebPage\\\",\\\"name\\\":\\\"标签: 基础\\\"}\"]]},\"headers\":[],\"git\":{},\"readingTime\":{\"minutes\":0,\"words\":0},\"filePathRelative\":null,\"excerpt\":\"\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
