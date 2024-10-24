c0a02034707ac599884fdb1e0fa72bac9cfafd16
bigshcool <990194923@qq.com>
2024-10-25 02:10:53 +0800

    修改配置

--- src/.vuepress/.temp/blog/type.js
+++ src/.vuepress/.temp/blog/type.js
@@ -1,4 +1,4 @@
-export const typesMap = {"article":{"/":{"path":"/article/","indexes":[0,1,2,3,4,5,6,7,8,24,11,19,18,17,27,28,20,21,22,23,25,26,29,9,12,13,16,14,15,10]}},"star":{"/":{"path":"/star/","indexes":[20,0,1,2,3,4,5,6,7,8,24,25,11]}},"timeline":{"/":{"path":"/timeline/","indexes":[0,1,2,3,4,5,6,7,8,24,19,18,17,27,28,20,21,22,23,25,26,11]}}};
+export const typesMap = {"article":{"/":{"path":"/article/","indexes":[6,7,8,9,10,11,12,13,14,24,0,19,18,17,27,28,20,21,22,23,25,26,29,1,2,5,3,4,15,16]}},"star":{"/":{"path":"/star/","indexes":[20,6,7,8,9,10,11,12,13,14,24,25,0]}},"timeline":{"/":{"path":"/timeline/","indexes":[6,7,8,9,10,11,12,13,14,24,19,18,17,27,28,20,21,22,23,25,26,0]}}};
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept();
